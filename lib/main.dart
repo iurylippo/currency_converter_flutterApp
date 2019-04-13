75c2a156f119a5f3da8ea9048d13ae790bb2931f
iuryvieira <iuryvieira.lippo@gmail.com>
2019-04-13 15:46:47 -0300

    add stateful widget with scaffold bar

--- lib/main.dart
+++ lib/main.dart
@@ -16,3 +16,22 @@ Future<Map> getData() async {
   return json.decode(response.body);
 }
 
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text("\$Conversor"),
+        backgroundColor: Colors.amber,
+        centerTitle: true,
+      ),
+      
+    );
+  }
+}
