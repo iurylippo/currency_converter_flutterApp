4bd0cde6d95454bfc8bc24ca03c72823d107f923
iuryvieira <iuryvieira.lippo@gmail.com>
2019-04-13 16:40:49 -0300

    add the snapshot data and the money icon

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,9 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  double dolar;
+  double euro;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -60,8 +63,19 @@ class _HomeState extends State<Home> {
                   ),
                 );
               } else {
-                return Container(
-                  color: Colors.green,
+                dolar = snapshot.data["results"]["currencies"]["USD"]["buy"];
+                euro = snapshot.data["results"]["currencies"]["EUR"]["buy"];
+                return SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      Icon(
+                        Icons.monetization_on,
+                        size: 150.0,
+                        color: Colors.amber,
+                      )
+                    ],
+                  ),
                 );
               }
           }
